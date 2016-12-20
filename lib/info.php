6cc1ba8d462f6c74cee560f8b9d871fe31c3e1fa
Herry Verdyansyah <kulme@icloud.com>
2016-12-21 05:18:19 +0700

    add more info

--- lib/info.php
+++ lib/info.php
@@ -8,13 +8,24 @@
 		$bitcoin->setSSL($config['rpc_ssl_ca']);
 	}
 	
-	$info = $bitcoin->getinfo();
-	
 	$main_address = $bitcoin->getaccountaddress('');
 	
 	$wallet_info = $bitcoin->getwalletinfo();
 	
+	$total_connection = $bitcoin->getconnectioncount();
+	
+	$mempool_total = $bitcoin->getmempoolinfo();
+	
 	$blockchain_info = $bitcoin->getblockchaininfo();
+	
+	$byte_recv_sent_info = $bitcoin->getnettotals();
+
+	function convert_byte($size){
+		$base = log($size) / log(1024);
+		$suffix = array("Bytes", "KB", "MB", "GB", "TB");
+		$f_base = floor($base);
+		return round(pow(1024, $base - floor($base)), 1) . " " . $suffix[$f_base];
+	}
 	
 	$btc_amount = $wallet_info['balance'];
 	$fiat_type = $config['fiat_type'];
--- lib/reload.php
+++ lib/reload.php
@@ -1,9 +1,13 @@
 <?php require_once 'config.php'; require_once 'info.php'; ?>
-<?php if($bitcoin->error){ echo "<p class='lead bg-warning'><strong>It seems error with the Bitcoin Daemon.<br>Bitcoin response message: " . $bitcoin->error . "<br><br>Please fix and try again.</strong></p>"; } else { ?>
+<?php if($bitcoin->error){ echo "<p class='lead bg-warning'><strong>There was a problem while connecting to your $config[coin_name] Daemon.<br>$config[coin_name] response message: " . $bitcoin->error . "<br><br>Please fix and try again.</strong></p>"; } else { ?>
 <p class="lead"><strong>Main Address: <?php echo $main_address; ?></strong></p>
 <p class="lead"><strong>Balance: <?php echo $wallet_info['balance']; ?> <?php echo $config['coin_symbol']; ?><?php if ($config['show_fiat'] === true) { $bitcoin_fiat = file_get_contents($price_url); echo " | ". $bitcoin_fiat; } ?></strong></p>
 <p class="lead"><strong>Unconfirmed Balance: <?php echo $wallet_info['unconfirmed_balance']; ?> <?php echo $config['coin_symbol']; ?></strong></p>
-<p class="lead"><strong>Connections: <?php echo $info['connections']; ?></strong></p>
+<p class="lead"><strong>Total Transactions: <?php echo $wallet_info['txcount']; ?></strong></p>
+<p class="lead"><strong>Connections: <?php echo $total_connection; ?></strong></p>
+<p class="lead"><strong>Total Transactions in Memory Pool: <?php echo $mempool_total['size']; ?></strong></p>
 <p class="lead"><strong>Blocks Height: <?php echo $blockchain_info['blocks']; ?></strong></p>
 <p class="lead"><strong>Blockchain Height: <?php echo $blockchain_info['headers']; ?></strong></p>
+<p class="lead"><strong>Network Sent: <?php echo convert_byte($byte_recv_sent_info['totalbytessent']); ?></strong></p>
+<p class="lead"><strong>Network Received: <?php echo convert_byte($byte_recv_sent_info['totalbytesrecv']); ?></strong></p>
 <?php } ?>
